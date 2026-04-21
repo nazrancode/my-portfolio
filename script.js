62600f8754e8d6f764b5eef53390f263b7de2666
Nazran Arfan <nazran.code@gmail.com>
2026-04-21 22:11:16 +0800

    complete basic portfolio v1

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ document.querySelectorAll("a").forEach(anchor => {
       });
   });
 });
+
+// Smooth scroll
+document.querySelectorAll("a").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({
+        behavior: "smooth"
+      });
+  });
+});
+
+// Button actions
+function viewCode() {
+  alert("This will link to GitHub project soon");
+}
+
+function contactMe() {
+  alert("Email me at your-email@gmail.com");
+}
+
+// Page load log
+console.log("Portfolio Loaded");
