580a5892420a54fc0d9eb539362a0c958bfdaf2f
Nazran Arfan <nazran.code@gmail.com>
2026-04-21 21:55:41 +0800

    adding navigatio bar and smooth scrolling

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
 function showMessage() {
   alert("Thanks for visiting my portfolio!");
 }
+
+//
+
+document.querySelectorAll("a").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({
+        behavior: "smooth"
+      });
+  });
+});
