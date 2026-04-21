d2715c11c0221794f07c4ca019fe056a3e0c1357
Nazran Arfan <nazran.code@gmail.com>
2026-04-21 21:48:41 +0800

    adding basic interactive with Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Portfolio loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Welcome to Nazran Arfan's Portfolio");
+});
+
+function showMessage() {
+  alert("Thanks for visiting my portfolio!");
+}
